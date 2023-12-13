1691d699ae01c471c00150a30b83f18a74e9871d
Joao Lozi <joaolozi@gmail.com>
2023-12-13 02:01:30 -0400

    Almost Done

---
+++ src/main/java/com/Lozi/Todo/List/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.Lozi.Todo.List.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+}
